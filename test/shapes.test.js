373b4da16571e01a140ec18552eddc4b083244d5
Grant-Ellington <gellingtonem6@gmail.com>
2023-04-26 14:00:00 -0500

    wrote test

--- test/shapes.test.js
+++ test/shapes.test.js
@@ -2,17 +2,11 @@ const { Square, Triangle, Circle } = require("../lib/shapes");
 
 describe("Circle", () => {
   it("should render svg for a green circle element", () => {
-    const expectedSvg = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+    let expectedSvg = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg"><circle cx="150" cy="100" r="80" fill="green" /><text x="150" y="125" font-size="60" text-anchor="middle" fill="white">SVG</text></svg>`;
 
-<circle cx="150" cy="100" r="80" fill="green" />
-  
-<text x="150" y="125" font-size="60" text-anchor="middle" fill="white">SVG</text>
-  
-</svg>`;
+    let newInstance = new Circle('SVG', 'white', 'green').render()
 
-    let newInstance = new Circle('SVG', 'white', 'green')
-
-    expect(newInstance.render()).toMatch(expectedSvg)
+    expect(newInstance).toMatch(expectedSvg)
 
 
 
@@ -22,28 +16,24 @@ describe("Circle", () => {
 
 describe("Triangle", () => {
   it("should render svg for a green polygon element", () => {
-    const expectedSvg =
-      '<polygon points="150, 18 244, 182 56, 182" fill="bisque" />';
+    let expectedSvg = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg"><polygon points="150, 18 244, 182 56, 182" fill="green" /><text x="150" y="150" font-size="60" text-anchor="middle" fill="white">text</text></svg>`;
+
+    let newInstance = new Triangle('text', 'white', 'green').render()
+
+    expect(newInstance).toMatch(expectedSvg)
+  });
 
 
   });
-  it("should accept a fillColor param", () => {
-    const expectedSvg =
-      '<polygon points="150, 18 244, 182 56, 182" fill="purple" />';
-
-
-  });
-});
 
 describe("Square", () => {
   it("should render svg for a green polygon element", () => {
-    const expectedSvg =
-      '<rect x="90" y="40" width="120" height="120" fill="dodgerblue" />';
+    let expectedSvg =`<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg"><rect x="90" y="40" width="120" height="120" fill="green"/><text x="150" y="125" font-size="60" text-anchor="middle" fill="white">text</text></svg>`;
+
+    let newInstance = new Square('text', 'white', 'green').render();
+    
+    expect(newInstance).toMatch(expectedSvg)
 
   });
-  it("should accept a fillColor param", () => {
-    const expectedSvg =
-      '<rect x="90" y="40" width="120" height="120" fill="red" />';
-
-  });
+  
 });
